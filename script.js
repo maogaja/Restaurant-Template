c25337df27959f0f412582382df8842a475b96d5
Maalin Ogaja <121969650+maogaja@users.noreply.github.com>
2024-02-24 12:48:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+//Tabbed Menu?
+/*
+function openMenu(event, menuName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("menu");
+    let menuArray = document.getElementsByClassName("menu");
+    for ( i = 0; i < x.length; i++) {
+        x[i].style.display = 'none';
+    }
+
+    tablinks = document.getElementsByClassName('tablinks');
+    for ( i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove('active-tab');
+    }
+
+    document.getElementById(menuName).style.display = 'block';
+    event.currentTarget.classList.add('active-tab');
+}
+
+document.getElementById("mainLink").click();*/
